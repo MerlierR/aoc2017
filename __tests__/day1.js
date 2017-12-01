ecbeb4993fa192906490f26049a4c89ea86fc47e
MerlierR <robbert.merlier@axians.com>
2017-12-01 14:11:55 +0100

    Day 1: Part 2

--- __tests__/day1.js
+++ __tests__/day1.js
@@ -1,14 +1,26 @@
 const captcha = require('../day1/captcha');
 
 describe('Day 1: Inverse Captcha', () => {
-    const data = [
+    const dataPart1 = [
         { input: '1122', result: 3 },
         { input: '1111', result: 4 },
         { input: '1234', result: 0 },
         { input: '91212129', result: 9 }
     ];
 
-    data.forEach((entry) => it(`has the correct result (${entry.result}) for "${entry.input}"`, () => {
+    dataPart1.forEach((entry) => it(`has the correct result (${entry.result}) for "${entry.input}"`, () => {
+        expect(captcha(entry.input, 1)).toBe(entry.result);
+    }));
+
+    const dataPart2 = [
+        { input: '1212', result: 6 },
+        { input: '1221', result: 0 },
+        { input: '123425', result: 4 },
+        { input: '123123', result: 12 },
+        { input: '12131415', result: 4 }
+    ];
+
+    dataPart2.forEach((entry) => it(`has the correct result (${entry.result}) for "${entry.input}"`, () => {
         expect(captcha(entry.input)).toBe(entry.result);
     }));
 });
--- day1/captcha.js
+++ day1/captcha.js
@@ -1,13 +1,12 @@
-module.exports = function captcha(input) {
-    const adjustedInput = input + input.substr(0, 1);
+module.exports = function captcha(input, step = input.length / 2) {
     const length = input.length;
 
     let result = 0;
 
     let i = 0;
     for (i; i < length; i++) {
-        if (adjustedInput[i] === adjustedInput[i + 1]) {
-            result += parseInt(adjustedInput[i], 10);
+        if (input[i] === input[(i + step) % length]) {
+            result += parseInt(input[i], 10);
         }
     }
 
