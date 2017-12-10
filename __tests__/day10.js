196a1e7c264efe19b6d1b3cc6e61ff5fc67161ac
MerlierR <robbert.merlier@axians.com>
2017-12-10 14:08:07 +0100

    Day 10, part 2

--- __tests__/day10.js
+++ __tests__/day10.js
@@ -1,7 +1,36 @@
-const { hash } = require('../day10/knotHash');
+const { hash, parseInputAsCharList, parseInputAsNumberList, calculateDenseHash, hashToString } = require('../day10/knotHash');
 
 describe('Day 10', () => {
     it('can hash', () => {
-        expect(hash([3, 4, 1, 5], 5)).toEqual([3, 4, 2, 1, 0]);
+        expect(hash([3, 4, 1, 5], 1, 5, []).sparseHash).toEqual([3, 4, 2, 1, 0]);
     });
+
+    it('can parse the input as a number list', () => {
+        expect(parseInputAsNumberList('1,2,3')).toEqual([1, 2, 3]);
+    });
+
+    it('can parse the input as a char list', () => {
+        expect(parseInputAsCharList('1,2,3')).toEqual([49, 44, 50, 44, 51]);
+    });
+
+    it('can calculate dense hashes', () => {
+        expect(calculateDenseHash([65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22])).toEqual([64]);
+        expect(calculateDenseHash([
+            65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22,
+            65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22
+        ])).toEqual([64, 64]);
+    });
+
+    it('can convert a hash to string', () => {
+        expect(hashToString([64, 7, 255])).toBe('4007ff');
+    });
+
+    [
+        { input: '', expectedResult: 'a2582a3a0e66e6e86e3812dcb672a272' },
+        { input: 'AoC 2017', expectedResult: '33efeb34ea91902bb2f59c9920caa6cd' },
+        { input: '1,2,3', expectedResult: '3efbe78a8d82f29979031a4aa0b16a9d' },
+        { input: '1,2,4', expectedResult: '63960835bcdc130f0b66d7ff4f6a5a8e' }
+    ].forEach((entry) => it(`Can calculate the hash (${entry.expectedResult}) for "${entry.input}"`, () => {
+        expect(hash(parseInputAsCharList(entry.input)).result).toBe(entry.expectedResult);
+    }));
 });
--- day10/knotHash.js
+++ day10/knotHash.js
@@ -1,25 +1,64 @@
-function hash(/**number[]*/ input, /**number*/ listSize = 256) {
+const SUFFIX = [17, 31, 73, 47, 23];
+
+function parseInputAsNumberList(data) {
+    return data.split(',').map((n) => parseInt(n, 10));
+}
+
+function parseInputAsCharList(data) {
+    return data.split('').map((c) => c.charCodeAt());
+}
+
+function hash(/**number[]*/ input, /**number*/ rounds = 64, /**number*/ listSize = 256, suffix = SUFFIX) {
     let list = [...new Array(listSize).keys()];
     let skipSize = 0;
     let index = 0;
 
-    input.forEach((length) => {
-        // reverse list
-        const range = [...new Array(length).keys()];
-        const reversedElements = range.reduce((acc, i) => {
-            const element = list[(index + i) % listSize];
-            acc.push(element);
-            return acc;
-        }, []).reverse();
+    [...new Array(rounds).keys()].forEach((roundNumber) => {
+        input.concat(...suffix).forEach((length) => {
+            // reverse list
+            const range = [...new Array(length).keys()];
+            const reversedElements = range.reduce((acc, i) => {
+                const element = list[(index + i) % listSize];
+                acc.push(element);
+                return acc;
+            }, []).reverse();
 
-        range.forEach((i) => list[(index + i) % listSize] = reversedElements[i]);
+            range.forEach((i) => list[(index + i) % listSize] = reversedElements[i]);
 
-        // update indices
-        index = (index + length + skipSize) % listSize;
-        skipSize += 1;
+            // update indices
+            index = (index + length + skipSize) % listSize;
+            skipSize += 1;
+        });
     });
 
-    return list;
+    const denseHash = calculateDenseHash(list);
+    return {
+        sparseHash: list,
+        denseHash: denseHash,
+        result: hashToString(denseHash)
+    };
 }
 
-module.exports = { hash };
+function calculateDenseHash(/**number[]*/ input) {
+    const copy = [...input];
+    const result = [];
+
+    while (copy.length > 0) {
+        const batch = copy.splice(0, 16);
+        result.push(batch.reduce((acc, n) => acc ^ n, 0));
+    }
+
+    return result;
+}
+
+function hashToString(/**number[]*/ input) {
+    return input.reduce((acc, num) => acc + num.toString(16).padStart(2, '0'), '');
+}
+
+module.exports = {
+    hash,
+    calculateDenseHash,
+    hashToString,
+    parseInputAsNumberList,
+    parseInputAsCharList
+};
