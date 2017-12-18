e24f38253ac92e731e75cd8a7c1c2b6081e637d4
MerlierR <robbert.merlier@axians.com>
2017-12-18 16:50:38 +0100

    Remove brol

--- __tests__/day18.js
+++ __tests__/day18.js
@@ -1,28 +1,6 @@
 const InstructionSet = require('../day18/InstructionSet');
 
 describe('Day 18: Duet', () => {
-    it('can outputs the first receive instruction', async () => {
-        const input = [
-            'set a 1',
-            'add a 2',
-            'mul a a',
-            'mod a 5',
-            'snd a',
-            'set a 0',
-            'rcv a',
-            'jgz a -1',
-            'set a 1',
-            'jgz a -2'
-        ];
-
-        const [is0, is1] = InstructionSet.parseInput(input);
-        const result = await Promise.all([is0.execute(), is1.execute()])
-
-        expect(result.length).toBe(2);
-        expect(result[0]).toBe(1);
-        expect(result[1]).toBe(1);
-    });
-
     it('can do the math', async () => {
         const input = [
             'set i 31',
